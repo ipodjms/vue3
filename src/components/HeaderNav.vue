cd86c83fc34081bc349396176b42f39aef6a081a
Joao <jhoudoris@gmail.com>
2021-01-26 00:03:59 -0300

    separando componente de navegacao

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'headernav',  
+}
+</script>
+
+<style scoped>
+#nav {
+  display: flex;
+  justify-content: center;
+  padding: 30px;  
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 10px;
+}
+#nav a.vue-school-active-class {
+  color: #ab26ab;
+}
+</style>
