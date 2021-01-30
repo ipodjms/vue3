1e76dbc5bb757911e377b21d2aaa60fd3aafa5e0
Joao <jhoudoris@gmail.com>
2021-01-30 19:47:47 -0300

    colocando o header que recebe as inf dinamicas no local correto

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <Header />
+  
   <headernav />
   <router-view :key="$route.path" />
 </template>
 
 <script>
 import headernav from "@/components/HeaderNav";
-import Header from "@/components/Header";
 
 export default {
   components: {
     headernav,
-    Header
   }
 };
 </script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App" />    
+      <Header />
+      
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header";
+
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 };
 </script>
