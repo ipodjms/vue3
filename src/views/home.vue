f56312d6be08fdb4fcce900089e1200c194f14fb
Joao <jhoudoris@gmail.com>
2021-02-06 11:59:09 -0300

    componente timeline e componente loop para renderizar a lista

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App" />    
-      <Header />
-      
+    <Header />
+    <TimeLine />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import Header from "@/components/Header";
+import Header from "@/components/Header"
+import TimeLine from "@/components/TimeLine"
 
 export default {
   name: "home",
   components: {
     HelloWorld,
-    Header
+    Header,
+    TimeLine
   }
 };
 </script>
